b8d7bf3efdc8ddde4e0017a8a84d69d0078671a9
kamitaqu <80474726+kamitaqu@users.noreply.github.com>
2021-06-14 17:11:37 +0300

    Add files via upload

---
+++ Captcha/Captcha/CaptchaGen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Captcha
+{
+    class CaptchaGen
+    {
+        const string Symbols = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+        private int quantity;
+        private string captcha;
+        //Set quantity of letters in captcha
+        public void SetQuantity(int value)
+        {
+            quantity = value;
+        }
+        //Generate captcha with required quantity of lettes
+        public void Gen()
+        {
+            captcha = "";
+            Random r = new Random();
+            char[] symbols = Symbols.ToCharArray();
+            for (int i=0; i<quantity;i++)
+            {
+                captcha += symbols[r.Next(0, symbols.Length - 1)];
+            }
+        }
+        // Get captcha so we can show it in text or label
+        public string GetCaptcha()
+        {
+            return captcha;
+        }
+        // Method for match checking our captcha
+        public bool CheckCaptcha(string text)
+        {
+            if (captcha == text)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Captcha/Captcha/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Captcha
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        CaptchaGen captcha = new CaptchaGen();
+        private void gen_Click(object sender, RoutedEventArgs e) //Generating our captcha and show it in label
+        {
+            try //Need this so this programm wont stop if we didnt input parameters
+            {
+                captcha.SetQuantity(Convert.ToInt32(qt.Text));
+                captcha.Gen();
+                res.Content = captcha.GetCaptcha();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Type quantity in numbers");
+            }
+        }
+        private void check_Click(object sender, RoutedEventArgs e) // Basically checking is our captcha right or wr0ng and show messages
+        {
+            try //Need this so this programm wont stop if we didnt input parameters
+            {
+                string check = check1.Text;
+                if (captcha.CheckCaptcha(check) == true)
+                    MessageBox.Show("Captcha is right");
+                else if (captcha.CheckCaptcha(check) == false)
+                    MessageBox.Show("Captcha is wrong");
+            }
+            catch
+            {
+                MessageBox.Show("Write answer");
+            }
+        }
+    }
+}
